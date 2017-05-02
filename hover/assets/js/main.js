e209cea977a2397355b912b97188265f80908372
JosephSalzmanBye <jmsal6@student.monash.edu>
2017-05-02 11:06:57 +1000

    Creating Hover: research and Concept

---
+++ hover/assets/js/main.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+
+  $('.text1').mouseenter(function() {
+    $('.Smiley').addClass('showme');
+
+  });
+
+  $('.text1').mouseleave(function() {
+  $('.Smiley').removeClass('showme');
+  });
+
+  $('.SmileyEmojiImg').mouseenter(function() {
+    $('.SmileyEmojiTxt').toggleClass('showme');
+    $('.MellonImgTxt').removeClass('showme');
+     $('.MellonImg').removeClass('enlarge');
+      $(this).toggleClass('enlarge');
+  });
+
+  $('.MellonImg').mouseenter(function() {
+    $('.MellonImgTxt').toggleClass('showme');
+    $('.SmileyEmojiTxt').removeClass('showme');
+     $('.SmileyEmojiImg').removeClass('enlarge');
+      $(this).toggleClass('enlarge');
+
+  });
+  $('.MellonImgTxt').mouseenter(function() {
+      $('.SubTitle').toggleClass('showme');
+  });
+  $('.MellonImgTxt').mouseleave(function() {
+      $('.SubTitle').toggleClass('showme');
+  });
+
+  $('.thumbnail').click(function() {
+     $(this).toggleClass('enlarge');
+   });
+
+
+ $('.SmileyEmojiImg').mouseenter(function() {
+
+   });
+
+  $('.MellonImg').mouseenter(function() {
+
+     });
+
+
+
+});
