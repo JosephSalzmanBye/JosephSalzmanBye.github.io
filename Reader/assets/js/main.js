5333b1abb91d1e3f51bd767542e34a0e123d7ea2
JosephSalzmanBye <jmsal6@student.monash.edu>
2017-05-22 16:36:58 +1000

    creating response box and coding visibility

--- Reader/assets/js/main.js
+++ Reader/assets/js/main.js
@@ -1,62 +1,22 @@
   $(document).ready(function() {
 
-  $('.text1').mouseenter(function() {
-    $('.Smiley').addClass('showme');
-
-  });
-
-  $('.text1').mouseleave(function() {
-  $('.Smiley').removeClass('showme');
-  });
-
-  $('.inlineimage').mouseenter(function() {
-    $('.popup').removeClass('showme');
-    $('.inlineimage').removeClass('enlarge');
+/* create base function for expanding images */
+  $('.inlineImg').mouseenter(function() {
+    $('.responseBox').removeClass('show');
+    $('.inlineImg').removeClass('enlarge');
     $(this).toggleClass('enlarge');
   });
 
+/* Reading A prompts for responses */
 
-  $('.SmileyEmojiImg').mouseenter(function() {
-    $('.SmileyEmojiTxt').addClass('showme');
-  });
-
-  $('.MellonImg').mouseenter(function() {
-    $('.MellonImgTxt').addClass('showme');
+  $('.insert1').mouseenter(function() {
+    $('.insert1Response').addClass('show');
   });
 
 
 
 
-  $('.RickAstley').mouseenter(function() {
-      $(this).toggleClass('enlarge');
-  });
 
-  // $('.MellonImg').mouseenter(function() {
-  //   $('.MellonImgTxt').toggleClass('showme');
-  //   $('.SmileyEmojiTxt').removeClass('showme');
-  //    $('.SmileyEmojiImg').removeClass('enlarge');
-  //     $(this).toggleClass('enlarge');
-  //
-  // });
-  $('.MellonImgTxt').mouseenter(function() {
-      $('.SubTitle').toggleClass('showme');
-  });
-  $('.MellonImgTxt').mouseleave(function() {
-      $('.SubTitle').toggleClass('showme');
-  });
-
-  $('.thumbnail').click(function() {
-     $(this).toggleClass('enlarge');
-   });
-
-
- $('.SmileyEmojiImg').mouseenter(function() {
-
-   });
-
-  $('.MellonImg').mouseenter(function() {
-
-     });
 
 
 
