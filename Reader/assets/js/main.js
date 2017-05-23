80127413a6c7355f8b89a4180a115fe61c30391c
JosephSalzmanBye <jmsal6@student.monash.edu>
2017-05-23 14:58:26 +1000

    changed styling of pages

--- Reader/assets/js/main.js
+++ Reader/assets/js/main.js
@@ -127,12 +127,184 @@ $('.reference15').mouseenter(function() {
 });
 
 $('.reference15').mouseleave(function() {
-  $('.footnote15  ').removeClass('show');
+  $('.footnote15').removeClass('show');
 });
 
+/*ReadingC footnotes */
+$('.referenceC1').mouseenter(function() {
+  $('.footnoteC1').addClass('show');
+});
 
+$('.referenceC1').mouseleave(function() {
+  $('.footnoteC1  ').removeClass('show');
+});
+$('.referenceC2').mouseenter(function() {
+  $('.footnoteC2').addClass('show');
+});
 
+$('.referenceC2').mouseleave(function() {
+  $('.footnoteC2  ').removeClass('show');
+});
+$('.referenceC3').mouseenter(function() {
+  $('.footnoteC3').addClass('show');
+});
 
+$('.referenceC3').mouseleave(function() {
+  $('.footnoteC3  ').removeClass('show');
+});
+$('.referenceC4').mouseenter(function() {
+  $('.footnoteC4').addClass('show');
+});
 
+$('.referenceC4').mouseleave(function() {
+  $('.footnoteC4').removeClass('show');
+});
+$('.referenceC5').mouseenter(function() {
+  $('.footnoteC5').addClass('show');
+});
+
+$('.referenceC5').mouseleave(function() {
+  $('.footnoteC5').removeClass('show');
+});
+$('.referenceC6').mouseenter(function() {
+  $('.footnoteC6').addClass('show');
+});
+
+$('.referenceC6').mouseleave(function() {
+  $('.footnoteC6').removeClass('show');
+});
+$('.referenceC7').mouseenter(function() {
+  $('.footnoteC7').addClass('show');
+});
+
+$('.referenceC7').mouseleave(function() {
+  $('.footnoteC7').removeClass('show');
+});
+$('.referenceC8').mouseenter(function() {
+  $('.footnoteC8').addClass('show');
+});
+
+$('.referenceC8').mouseleave(function() {
+  $('.footnoteC8').removeClass('show');
+});
+$('.referenceC9').mouseenter(function() {
+  $('.footnoteC9').addClass('show');
+});
+
+$('.referenceC9').mouseleave(function() {
+  $('.footnoteC9').removeClass('show');
+});
+$('.referenceC10').mouseenter(function() {
+  $('.footnoteC10').addClass('show');
+});
+
+$('.referenceC10').mouseleave(function() {
+  $('.footnoteC10').removeClass('show');
+});
+$('.referenceC11').mouseenter(function() {
+  $('.footnoteC11').addClass('show');
+});
+
+$('.referenceC11').mouseleave(function() {
+  $('.footnoteC11').removeClass('show');
+});
+$('.referenceC12').mouseenter(function() {
+  $('.footnoteC12').addClass('show');
+});
+
+$('.referenceC12').mouseleave(function() {
+  $('.footnoteC12').removeClass('show');
+});
+$('.referenceC13').mouseenter(function() {
+  $('.footnoteC13').addClass('show');
+});
+
+$('.referenceC13').mouseleave(function() {
+  $('.footnoteC13').removeClass('show');
+});
+$('.referenceC14').mouseenter(function() {
+  $('.footnoteC14').addClass('show');
+});
+
+$('.referenceC14').mouseleave(function() {
+  $('.footnoteC14').removeClass('show');
+});
+$('.referenceC15').mouseenter(function() {
+  $('.footnoteC15').addClass('show');
+});
+
+$('.referenceC15').mouseleave(function() {
+  $('.footnoteC15').removeClass('show');
+});
+$('.referenceC16').mouseenter(function() {
+  $('.footnoteC16').addClass('show');
+});
+
+$('.referenceC16').mouseleave(function() {
+  $('.footnoteC16').removeClass('show');
+});
+$('.referenceC17').mouseenter(function() {
+  $('.footnoteC17').addClass('show');
+});
+
+$('.referenceC17').mouseleave(function() {
+  $('.footnoteC17').removeClass('show');
+});
+$('.referenceC18').mouseenter(function() {
+  $('.footnoteC18').addClass('show');
+});
+
+$('.referenceC18').mouseleave(function() {
+  $('.footnoteC18').removeClass('show');
+});
+$('.referenceC19').mouseenter(function() {
+  $('.footnoteC19').addClass('show');
+});
+
+$('.referenceC19').mouseleave(function() {
+  $('.footnoteC19').removeClass('show');
+});
+$('.referenceC20').mouseenter(function() {
+  $('.footnoteC20').addClass('show');
+});
+
+$('.referenceC20').mouseleave(function() {
+  $('.footnoteC20').removeClass('show');
+});
+$('.referenceC21').mouseenter(function() {
+  $('.footnoteC21').addClass('show');
+});
+
+$('.referenceC21').mouseleave(function() {
+  $('.footnoteC21').removeClass('show');
+});
+$('.referenceC22').mouseenter(function() {
+  $('.footnoteC22').addClass('show');
+});
+
+$('.referenceC22').mouseleave(function() {
+  $('.footnoteC22').removeClass('show');
+});
+$('.referenceC23').mouseenter(function() {
+  $('.footnoteC23').addClass('show');
+});
+
+$('.referenceC23').mouseleave(function() {
+  $('.footnoteC23').removeClass('show');
+});
+$('.referenceC24').mouseenter(function() {
+  $('.footnoteC24').addClass('show');
+});
+
+$('.referenceC24').mouseleave(function() {
+  $('.footnoteC24').removeClass('show');
+});
+$('.referenceC25').mouseenter(function() {
+  $('.footnoteC25').addClass('show');
+});
+
+$('.referenceC25').mouseleave(function() {
+  $('.footnoteC25').removeClass('show');
+});
 
 });
