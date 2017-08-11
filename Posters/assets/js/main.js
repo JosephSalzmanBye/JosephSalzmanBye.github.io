2bde992b9ef7b785ca6fdfa0f45ee313bba852b5
JosephSalzmanBye <jmsal6@student.monash.edu>
2017-08-11 14:53:21 +1000

    adding assets to new location

---
+++ Posters/assets/js/main.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+
+
+// Add jQuery here
+
+
+$(document).ready(function() {
+  $('.rowRed').addClass('startPosRed');
+  $('.rowBlue').addClass('startPosBlue');
+  $('.rowBlack').addClass('startPosBlack');
+  $('.txtTitle').addClass('startSizeTxt');
+});
+
+setTimeout(function(){
+    $('.txtTitle').addClass('endSizeTxt');
+    $('.txtBody').removeClass('startSizeTxt');
+  }
+
+  ,4000);
+setTimeout(function(){
+    $('.rowBlue').removeClass('startPosBlue');
+    $('.rowBlue').addClass('slideLeftBlue');
+  }
+
+  ,2950);
+setTimeout(function(){
+  $('.rowBlack').removeClass('startPosBlack');
+    $('.rowBlack').addClass('slideLeft');
+  }
+
+  ,2500);
+
+setTimeout(function(){
+  $('.rowRed').removeClass('startPosRed');
+    $('.rowRed').addClass('slideLeft');
+  }
+
+  ,2000);
+
+  $(".block").click(function(){
+    $('.diamond').addClass('round')
+  });
+
+
+
+
+
+
+
+
+
+  });
