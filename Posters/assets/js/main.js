3353905a3d1effbc90f34e2925fc86f2bc92f52b
JosephSalzmanBye <jmsal6@student.monash.edu>
2017-08-24 17:03:58 +1000

    completed posters, and links to each.

--- Posters/assets/js/main.js
+++ Posters/assets/js/main.js
@@ -3,7 +3,91 @@ $(document).ready(function() {
 
 // Add jQuery here
 
+/* posterd*/
+$(document).ready(function() {
+  $('.blockBlue').addClass('size');
+  $('.blockBlack').addClass('size');
+  $('.blockYellow').addClass('size');
+  $('.blockClear').addClass('size');
+  $('.blockGrey').addClass('size');
+  $('.blockRed').addClass('size');
+});
 
+$('.blockGrey').click(function(){
+
+  $('.vertical').toggleClass('oscillate');
+
+});
+$('.blockRed').click(function(){
+  $('.vertical .block').toggleClass('shrink');
+  $('.vertical .block').toggleClass('size');
+
+});
+$('.blockGrey').click(function(){
+  $('.vertical').toggleClass('shiftdown');
+
+});
+
+$('.blockBlack').click(function(){
+  $('.horizon .blockBlack').toggleClass('shiftDown');
+
+});
+$('.blockYellow').click(function(){
+  $('.horizon .blockYellow').toggleClass('shiftUp');
+
+});
+$('.blockClear').click(function(){
+    $('.blockClear').toggleClass('size');
+  $('.blockClear').toggleClass('grow');
+
+});
+$('.blockBlue').click(function(){
+  $('.horizon').toggleClass('shiftLeft');
+});
+
+/* postere*/
+var audioBell = document.createElement('audio');
+
+audioBell.setAttribute('src', 'http://freesound.org/data/previews/56/56076_692375-lq.mp3');
+$.get();
+
+audioBell.addEventListener("load", function() {
+  audioBell.play();
+}, true);
+
+var audioHorn = document.createElement('audio');
+
+audioHorn.setAttribute('src', 'https://www.soundjay.com/misc/hohner-melodica-1.mp3');
+$.get();
+
+audioHorn.addEventListener("load", function() {
+  audioHorn.play();
+}, true);
+
+$('.soundA').mouseenter(function() {
+  audioBell.play();
+  $('.circle.soundA').addClass('shake');
+});
+$('.soundA').mouseleave(function() {
+  audioBell.pause();
+    audioBell.currentTime = 0;
+  $('.circle.soundA').removeClass('shake');
+});
+$('.soundB').click(function() {
+  audioBell.pause();
+});
+$(".soundC").hover(
+  function() {
+    audioHorn.play();
+      $('.circle.soundC').addClass('moveLeft');
+  },
+  function() {
+    audioHorn.pause();
+    audioHorn.currentTime = 0;
+      $('.circle.soundC').removeClass('moveLeft');
+  }
+);
+/*Poster C */
 $(document).ready(function() {
   $('.rowRed').addClass('startPosRed');
   $('.rowBlue').addClass('startPosBlue');
--- assets/js/main.js
+++ assets/js/main.js
@@ -4,56 +4,45 @@ $(document).ready(function() {
 // Add jQuery here
 
 
-$(document).ready(function() {
-  $('.rowRed').addClass('startPosRed');
-  $('.rowBlue').addClass('startPosBlue');
-  $('.rowBlack').addClass('startPosBlack');
-  $('.txtTitle').addClass('startSizeTxt');
+
+$(".link1").mouseenter(function(){
+  $('.rect.orange').addClass('lift')
+  $('.preview .image1').addClass('show')
 });
-
-setTimeout(function(){
-    $('.txtTitle').addClass('endSizeTxt');
-    $('.txtBody').removeClass('startSizeTxt');
-  }
-
-  ,4000);
-setTimeout(function(){
-    $('.rowBlue').removeClass('startPosBlue');
-    $('.rowBlue').addClass('slideLeftBlue');
-  }
-
-  ,2950);
-setTimeout(function(){
-  $('.rowBlack').removeClass('startPosBlack');
-    $('.rowBlack').addClass('slideLeft');
-  }
-
-  ,2500);
-
-setTimeout(function(){
-  $('.rowRed').removeClass('startPosRed');
-    $('.rowRed').addClass('slideLeft');
-  }
-
-  ,2000);
-
-  $("block").click(function(){
-
-    $('.diamond').addClass('txtChange');
-    $('.txtTitle').removeClass('endSizeTxt');
-      $('.txtTitle').addClass('startSizeTxt');
-
-  });
-
-
-
-
-
-
-
-
-
-
-
-
-  });
+$(".link1").mouseleave(function(){
+  $('.rect.orange').removeClass('lift')
+  $('.preview .image1').removeClass('show')
+});
+$(".link2").mouseenter(function(){
+  $('.rect.blue').addClass('lift')
+    $('.preview .image2').addClass('show')
+});
+$(".link2").mouseleave(function(){
+  $('.rect.blue').removeClass('lift')
+    $('.preview .image2').removeClass('show')
+});
+$(".link3").mouseenter(function(){
+  $('.rect.green').addClass('lift')
+    $('.preview .image3').addClass('show')
+});
+$(".link3").mouseleave(function(){
+  $('.rect.green').removeClass('lift')
+    $('.preview .image3').removeClass('show')
+});
+$(".link4").mouseenter(function(){
+  $('.rect.yellow').addClass('lift')
+    $('.preview .image4').addClass('show')
+});
+$(".link4").mouseleave(function(){
+  $('.rect.yellow').removeClass('lift')
+    $('.preview .image4').removeClass('show')
+});
+$(".link5").mouseenter(function(){
+  $('.rect.red').addClass('lift')
+    $('.preview .image5').addClass('show')
+});
+$(".link5").mouseleave(function(){
+  $('.rect.red').removeClass('lift')
+    $('.preview .image5').removeClass('show')
+});
+});
