5190c55ac74a4197e147d24c7be75bf7a786dfff
jsbmisinformation <josephsalzmanbye@gmail.com>
2017-09-22 12:28:13 +1000

    managing page layout

--- archive/assets/js/main.js
+++ archive/assets/js/main.js
@@ -1,8 +1,53 @@
 $(document).ready(function(){
 
-$('.col1of5 .content').click(function(){
-  moon[0].mesh.material.uniforms.texture.value = THREE.ImageUtils.loadTexture("img2.png");
-  moon[0].mesh.material.uniforms.texture.needsUpdate = true;
-});
+  $('.nav_All').click(function(){
+    $('.item').removeClass('hide');
+    $('.item').addClass('show');
+  });
+
+  $('.nav_blue').click(function(){
+    $('.item').removeClass('show');
+    $('.item').addClass('hide');
+
+    $('.item.blue').removeClass('hide');
+    $('.item.blue').addClass('show')
+  });
+
+  $('.nav_green').click(function(){
+    $('.item').removeClass('show');
+    $('.item').addClass('hide');
+
+    $('.item.green').removeClass('hide');
+    $('.item.green').addClass('show')
+  });
+
+  $('.nav_brown').click(function(){
+    $('.item').removeClass('show');
+    $('.item').addClass('hide');
+
+    $('.item.brown').removeClass('hide');
+    $('.item.brown').addClass('show')
+  });
+
+  $('.nav_Red').click(function(){
+    $('.item').removeClass('show');
+    $('.item').addClass('hide');
+
+    $('.item.Red').removeClass('hide');
+    $('.item.Red').addClass('show')
+  });
+
+
+  $('.nav_Grey').click(function(){
+    $('.item').removeClass('show');
+    $('.item').addClass('hide');
+
+    $('.item.Grey').removeClass('hide');
+    $('.item.Grey').addClass('show')
+  });
+
+
+
+
 
 });
